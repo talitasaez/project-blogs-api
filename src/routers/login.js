abacfbe4cd206172ae89bac00d2f93ceb7fbcc89
talitasaez <tltsaez@gmail.com>
2023-01-27 20:53:08 -0300

    requisito 3 incompleto

--- src/routers/login.js
+++ src/routers/login.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const loginController = require('../controllers/loginController');
+const { loginValidate } = require('../middlewares/loginValidate');
 
 const router = express.Router();
 
-router.post('/login', loginController.postLogin);
+router.post('/login', loginValidate, loginController.postLogin);
 
 module.exports = router;
--- src/services/loginService.js
+++ src/services/loginService.js
@@ -1,16 +1,15 @@
 const { User } = require('../models');
 
 const postLogin = async (email, password) => {
-
-const userLogin = await User.findOne({ 
-    where: { email, password },
-});
-  if (!userLogin) return { type: 'error', message: 'Invalid fields' };
-
-  return { type: null };
-
+    const userLogin = await User.findOne({ 
+        where: { email, password },
+    });
+    
+      if (!userLogin) {
+        return { type: 400, message: 'Invalid fields' };
+      }
+    
+      return { type: null, message: '' };
 };
 
-module.exports = {
-postLogin,
-};
+module.exports = { postLogin };
