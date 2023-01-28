89ce6e46d6fece0e09284eaf3d80d4745ac4eb64
talitasaez <tltsaez@gmail.com>
2023-01-27 21:22:26 -0300

    requisito 3 incompleto

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -5,7 +5,8 @@ const { JWT_SECRET } = process.env;
 
 const postLogin = async (req, res) => {
   const { email, password } = req.body;
-  const { type, message } = await loginService.postLogin(email, password);
+  const login = await loginService.postLogin(email, password);
+  const { type, message } = login;
   if (type) return res.status(400).json({ message });
 
 const payload = { 
