7396cab84f15c01b84d78c9de500df2e727eb0ce
talitasaez <tltsaez@gmail.com>
2023-02-06 10:50:15 -0300

    req 13 finalmente ok

--- src/models/PostCategory.js
+++ src/models/PostCategory.js
@@ -1,37 +1,35 @@
-
-const post_category = (sequelize, DataTypes) => {
-    const PostCategory  =  sequelize.define('PostCategory', {
-     postId: {
-       type:  DataTypes.INTEGER,
-       primaryKey: true, 
-       foreingKey: true,
-     }, 
-     categoryId: {
-        type:  DataTypes.INTEGER,
-        primaryKey: true, 
-        foreingKey: true,
-      }, 
+const post_category = (sequelize,DataTypes) => {
+  const PostCategory = sequelize.define('PostCategory', {
+    postId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      foreingkey: true 
     },
-    {
-     tableName: 'posts_categories',
-     underscored: true,
-     timestamps: false, 
+    categoryId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      foreingkey: true 
+    },
+  },
+  {
+    timestamps: false,
+    tableName: 'posts_categories',
+    underscored: true,
+  });
+  PostCategory.associate = (models) => {
+    models.BlogPost.belongsToMany(models.Category, {
+      as: 'categories',
+      through: PostCategory,
+      foreignKey: 'postId',
+      otherKey: 'categoryId',
     });
-
-    PostCategory.associate = (models) => {
-        models.BlogPost.belongsToMany(models.Category, {
-            as: 'categories',
-            through: PostCategory,
-            foreingKey: 'postId',
-            otherKey: 'categoryId',
-        });
-        models.Category.belongsToMany(models.BlogPost, {
-            as: 'blog_posts',
-            through: PostCategory,
-            foreingKey: 'categoryId',
-            otherKey: 'postId',
-        });
-    }
+    models.Category.belongsToMany(models.BlogPost, {
+      as: 'blog_posts',
+      through: PostCategory,
+      foreignKey: 'categoryId',
+      otherKey: 'postId',
+    });
+  }
   return PostCategory;
  };
  
